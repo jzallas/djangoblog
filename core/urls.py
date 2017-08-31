109f917ef9533245b3ab9e7d556e14fca55bc9db
Jzallas <>
2017-08-31 15:50:46 -0400

    directed all paths to blog app renamed admin path

--- core/urls.py
+++ core/urls.py
@@ -18,7 +18,6 @@ from django.contrib import admin
 from django.contrib.auth import views
 
 urlpatterns = [
-    url(r'^blog/', include('blog.urls')),
-    url(r'^accounts/login/$', views.login, name='login'),
-    url(r'^admin/', admin.site.urls),
+    url(r'^', include('blog.urls')),
+    url(r'^console/', admin.site.urls),
 ]
